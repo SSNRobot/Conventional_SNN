2c6b564af6797ece2578d134668b5cedb7bbebc3
Phillip Salowe <64107928+pjsalowe@users.noreply.github.com>
2022-10-12 16:54:25 -0400

    Update RisingEdgeCounterV2.vhd

--- RisingEdgeCounterV2.vhd
+++ RisingEdgeCounterV2.vhd
@@ -3,14 +3,14 @@ use ieee.numeric_bit.all;
 
 entity RisingEdgeCounterV2 is
     port(clk, rst, en, l, r: in bit;
-        q_l, q_r : out bit_vector(13 downto 0));
+        q_l, q_r : out bit_vector(13 downto 0));       //These two are for the two outputs of the SNN
 end RisingEdgeCounterV2;
 
 architecture sequential of RisingEdgeCounterV2 is
     begin
     process(l, r)
         variable count1 : integer range 0 to 16000;
-          variable count2 : integer range 0 to 16000;
+          variable count2 : integer range 0 to 16000;   //max rising edges is 16000
     begin
         if rst = '0' then
             count1 := 0;
@@ -26,7 +26,7 @@ architecture sequential of RisingEdgeCounterV2 is
             count1 := count1;
                 count2 := count2;
         end if;
-				q_l <= bit_vector(to_unsigned(count1, 14));
-				q_r <= bit_vector(to_unsigned(count2, 14));
+	q_l <= bit_vector(to_unsigned(count1, 14));
+	q_r <= bit_vector(to_unsigned(count2, 14));
     end process;
 end sequential;
