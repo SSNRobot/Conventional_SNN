310fed4e8d3a47c8dbe177922d8f208f5f2a6e7a
Phillip Salowe <64107928+pjsalowe@users.noreply.github.com>
2023-03-30 16:37:18 -0400

    Update r_e_cntr_vhdl.vhd

--- r_e_cntr_vhdl.vhd
+++ r_e_cntr_vhdl.vhd
@@ -1,3 +1,7 @@
+-- Author: Phillip Salowe and Alex Pippin
+-- This code is a rising edge counter for a 50Mhz
+-- Last editted: 3/30/2023
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -16,22 +20,22 @@ architecture rtl of r_e_cntr_vhdl is
 begin
     process(inp, clk)
     begin
-        if rising_edge(inp) then
+        if rising_edge(inp) then   --count rising edges for the input signal
             cnt <= cnt + 1;
         end if;
 
-        if rising_edge(clk) then
+        if rising_edge(clk) then   --count how fast the clock is going
             ticks <= ticks + 1;
         end if;
 
-        if ticks = 750 then
+        if ticks = 10000000 then     --push amount of rising edges to output
             count <= std_logic_vector(cnt);
             ticks <= (others => '0');
         end if;
 		  
-			if ticks = 0 then
-				cnt <= (others => '0');
-			end if;
+	if ticks = 0 then   --reset cnt, which is variable that counts how fast clock is going
+		cnt <= (others => '0');
+	end if;
 		  
     end process;
 end architecture;
